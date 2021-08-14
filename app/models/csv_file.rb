045088a6e64a5b742a5e95fe5fd869ea9dc7ec94
samo2105 <samuel.cea95@gmail.com>
2021-08-14 13:39:46 -0400

    feat: added csv file validations

--- app/models/csv_file.rb
+++ app/models/csv_file.rb
@@ -1,4 +1,13 @@
 class CsvFile < ApplicationRecord
   belongs_to :user
   has_one_attached :file
+  validates :user_id, :file, :state, presence: true
+  enum state: [:on_hold, :processing, :failed, :finished]
+  validate :correct_file_mime_type
+
+  private
+
+  def correct_file_mime_type
+    errors.add(:file, 'File must be CSV format') if file.attached? && !file.content_type.in?(%w(application/csv))
+  end
 end
