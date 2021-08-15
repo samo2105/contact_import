537bc51558e03758002189a206cd1c7e9a67370a
samo2105 <samuel.cea95@gmail.com>
2021-08-14 20:08:52 -0400

    feat: added table to contacts view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,6 @@ class ContactsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @contacts = current_user.contacts.paginate(page: params[:page], per_page: 10)
+        @contacts = Contact.where(user_id: current_user.id).paginate(page: params[:page], per_page: 10)
     end
 end
